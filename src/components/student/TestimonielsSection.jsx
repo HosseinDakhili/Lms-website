017acc972fcd6cc141a94b63a0d095e9a31d0efd
HosseinDakhili <hosseindakhili94@gmail.com>
2025-07-05 23:18:12 +0330

    Add new SVG icons and components for educator and student pages
    - Added SVG icons: star_dull_icon, time_clock_icon, time_left_clock_icon, twitter_icon, upload_area, user_icon, user_icon_2, walmart_logo, favicon.
    - Created new components for educator: Footer, Navbar, Sidebar, AddCourses, Dashboard, Educator, MyCourses, StudentsEnrolled.
    - Created new components for student: CallToAction, Companies, CourseCard, CoursesSection, Footer, Hero, Loading, Navbar, Rating, SearchBar, TestimonielsSection, CourseDetails, CoursesList, Home, MyEnrollments, Player.
    - Updated index.css to include Google Fonts and removed unused keyframes.

---
+++ src/components/student/TestimonielsSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TestimonielsSection = () => {
+  return (
+    <div>
+      <h1>TestimonielsSection</h1>
+    </div>
+  )
+}
+
+export default TestimonielsSection
