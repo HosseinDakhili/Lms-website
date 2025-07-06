8a4b729ce9b0d84c40e0aa8005f4722c480a10c3
HosseinDakhili <hosseindakhili94@gmail.com>
2025-07-06 15:58:33 +0330

    Add new SVG icons and update Navbar component
    - Added new SVG icons: star_dull_icon, time_clock_icon, time_left_clock_icon, twitter_icon, upload_area, user_icon, user_icon_2, and walmart_logo.
    - Updated Navbar component to include logo and navigation links with conditional styling based on the current page.
    - Introduced AppContext file structure for potential context management.
    - Wrapped App component with BrowserRouter for routing capabilities.
    - Integrated Outlet component in Educator page for nested routing.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Student/Home";
+import CoursesList from "./pages/Student/CoursesList";
+import CourseDetails from "./pages/Student/CourseDetails";
+import MyEnrollments from "./pages/Student/MyEnrollments";
+import Player from "./pages/Student/Player";
+import Loading from "./components/student/Loading";
+import Educator from "./pages/Educator/Educator";
+import Dashboard from "./pages/Educator/Dashboard";
+import AddCourses from "./pages/Educator/AddCourses";
+import MyCourses from "./pages/Educator/MyCourses";
+import StudentsEnrolled from "./pages/Educator/StudentsEnrolled";
+import Navbar from "./components/student/Navbar";
 
 export default function App() {
   return (
-    <div>
-      
+    <div className="text-default min-h-screen bg-white">
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/course-list" element={<CoursesList />} />
+        <Route path="/course-list/input" element={<CoursesList />} />
+        <Route path="/course/:id" element={<CourseDetails />} />
+        <Route path="/my-enrollments" element={<MyEnrollments />} />
+        <Route path="/player/courseId" element={<Player />} />
+        <Route path="/loading/path" element={<Loading />} />
+        <Route path="/educator" element={<Educator />}>
+          <Route path="educator" element={<Dashboard />} />
+          <Route path="add-course" element={<AddCourses />} />
+          <Route path="my-course" element={<MyCourses />} />
+          <Route path="student-enrolled" element={<StudentsEnrolled />} />
+        </Route>
+      </Routes>
     </div>
-  )
+  );
 }
