9469c50857db8bc89c1b642635bf82673b8f1caf
HosseinDakhili <hosseindakhili94@gmail.com>
2025-07-10 00:35:19 +0330

    Add navigation functionality to Navbar and provide navigate context in AppContext

--- src/components/student/Navbar.jsx
+++ src/components/student/Navbar.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 import { assets } from "../../assets/assets";
 import { Link } from "react-router-dom";
 import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
+import { AppContext } from "../../context/AppContext";
 const Navbar = () => {
   const isCourseListPage = location.pathname.includes("/course-list");
   const { openSignIn } = useClerk();
   const { user } = useUser();
+  const {navigate} = useContext(AppContext)
   return (
     <nav
       className={`flex items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-400 py-4 ${
@@ -13,6 +15,7 @@ const Navbar = () => {
       }`}
     >
       <img
+      onClick={()=>navigate('/')}
         src={assets.logo}
         alt="Logo"
         className="w-28 lg:w-32 cursor-pointer"
--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,8 +1,10 @@
 import { createContext, useEffect, useState } from "react";
 import { dummyCourses } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
 export const AppContext = createContext();
 export const AppContextProvider = (props) => {
   const currency = import.meta.env.VITE_CURRENCY;
+  const navigate = useNavigate()
   const [allCourses,setAllCourses] = useState([])
   //Fetch all courses
   const fetchAllCourses = async () =>{
@@ -12,7 +14,7 @@ export const AppContextProvider = (props) => {
     fetchAllCourses()
   },[])
   const value = {
-    currency,allCourses
+    currency,allCourses,navigate
   };
   return (
     <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
