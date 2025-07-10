22f0d8f93d769165adbab1f74632948d693624a7
HosseinDakhili <hosseindakhili94@gmail.com>
2025-07-10 10:54:14 +0330

    Refactor CoursesList component to integrate AppContext and enhance layout with SearchBar and course display

--- src/pages/Student/CoursesList.jsx
+++ src/pages/Student/CoursesList.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { AppContext } from "../../context/AppContext";
+import SearchBar from "../../components/student/SearchBar";
+import { useParams } from "react-router-dom";
+import CourseCard from "../../components/student/CourseCard";
 
 const CoursesList = () => {
+  const { navigate, allCourses } = useContext(AppContext);
+  const { input } = useParams();
+  const courseItems = allCourses.map((course, index) => (
+    <CourseCard key={index} course={course} />
+  ));
   return (
-    <div>
-      <h1>CoursesList</h1>
-    </div>
-  )
-}
+    <>
+      <div className="relative md:px-36 px-8 pt-20 text-left">
+        <div className="flex md:flex-row flex-col gap-6 items-start justify-between w-full">
+          <div>
+            <h2 className="text-4xl font-semibold text-gray-800">
+              Course List
+            </h2>
+            <p className="text-gray-500">
+              <span
+                onClick={() => navigate("/")}
+                className="text-blue-600 cursor-pointer"
+              >
+                Home
+              </span>
+              / <span>Course List</span>
+            </p>
+          </div>
+          <SearchBar data={input} />
+        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 my-16 gap-3 px-2 md:p-0">
+          {courseItems}
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default CoursesList
+export default CoursesList;
