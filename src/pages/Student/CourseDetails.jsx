f4f45f3aa2f90780073940d3e5db6f07789bb66c
HosseinDakhili <hosseindakhili94@gmail.com>
2025-07-10 15:46:14 +0330

    Add Footer component and enhance CourseDetails layout with course pricing, enrollment button, and course details

--- src/pages/Student/CourseDetails.jsx
+++ src/pages/Student/CourseDetails.jsx
@@ -4,17 +4,20 @@ import { AppContext } from "../../context/AppContext";
 import Loading from "../../components/student/Loading";
 import { assets } from "../../assets/assets";
 import humanizeDuration from "humanize-duration";
+import Footer from "../../components/student/Footer";
 
 const CourseDetails = () => {
   const { id } = useParams();
   const [courseData, setCourseData] = useState(null);
   const [openSection, setopenSection] = useState({});
+  const [isAlreadyEnrolled, setisAlreadyEnrolled] = useState(false);
   const {
     allCourses,
     calculateRating,
     calculateChapterTime,
     calculateCourseDuration,
     calculateNoOfLectures,
+    currency,
   } = useContext(AppContext);
   const fetchCourseData = async () => {
     const findCourse = allCourses.find((course) => course._id == id);
@@ -28,7 +31,8 @@ const CourseDetails = () => {
   };
   if (!courseData) return <Loading />;
   return (
-    <div className="flex md:flex-row flex-col-reverse gap-10 relative items-start justify-between md:px-36 px-8 md:pt-30 pt-20 text-left">
+    <>
+    <section className="flex md:flex-row flex-col-reverse gap-10 relative items-start justify-between md:px-36 px-8 md:pt-30 pt-20 text-left">
       <div className="absolute top-0 left-0 w-full h-[500px] z-10 bg-gradient-to-b from-cyan-100/70"></div>
       {/* left col */}
       <div className="max-w-xl z-10 text-gray-500">
@@ -83,7 +87,13 @@ const CourseDetails = () => {
                   onClick={() => toggleSection(index)}
                 >
                   <div className="flex items-center gap-2">
-                    <img className={`transform transition-transform ${openSection[index]?'rotate-180':''}  `} src={assets.down_arrow_icon} alt="" />
+                    <img
+                      className={`transform transition-transform ${
+                        openSection[index] ? "rotate-180" : ""
+                      }  `}
+                      src={assets.down_arrow_icon}
+                      alt=""
+                    />
                     <p className="font-medium md:text-base text-sm">
                       {chapter.chapterTitle}
                     </p>
@@ -93,7 +103,11 @@ const CourseDetails = () => {
                     {calculateChapterTime(chapter)}
                   </p>
                 </div>
-                <div className={`overflow-hidden transition-all duration-300  ${openSection[index]?'max-h-96':'max-h-0'}`}>
+                <div
+                  className={`overflow-hidden transition-all duration-300  ${
+                    openSection[index] ? "max-h-96" : "max-h-0"
+                  }`}
+                >
                   <ul className="list-disc md:pl-10 pl-4 pr-4 py-2 text-gray-600 border-t border-gray-300">
                     {chapter.chapterContent.map((lecture, i) => (
                       <li className="flex items-start gap-2 py-1" key={i}>
@@ -131,19 +145,81 @@ const CourseDetails = () => {
           </div>
         </div>
         <div className="py-20 text-sm md:text-lg">
-          <h3 className="text-xl font-semibold text-gray-800">Course Description</h3>
-           <p
-          className="pt-3 rich-text"
-          dangerouslySetInnerHTML={{
-            __html: courseData.courseDescription
-          }}
-        ></p>
+          <h3 className="text-xl font-semibold text-gray-800">
+            Course Description
+          </h3>
+          <p
+            className="pt-3 rich-text"
+            dangerouslySetInnerHTML={{
+              __html: courseData.courseDescription,
+            }}
+          ></p>
         </div>
       </div>
       {/* right col */}
-      <div></div>
-    </div>
-  );
+      <div className="max-w-[424px]  z-10 shadow-lg rounded-t md:rounded-none overflow-hidden bg-white min-w-[300px] sm:min-w-[420px] ">
+        <img src={courseData.courseThumbnail} alt="" />
+        <div className="p-5">
+          <div className="flex items-center gap-2">
+            <img className="w-3.5" src={assets.time_left_clock_icon} alt="" />
+            <p className="text-red-500">
+              <span className="font-medium">5 days</span> left at this price!
+            </p>
+          </div>
+          <div className="flex gap-3 items-center pt-2">
+            <p className="text-gray-800 md:text-4xl text-2xl font-semibold">
+              {currency}
+              {(
+                courseData.coursePrice -
+                (courseData.discount * courseData.coursePrice) / 100
+              ).toFixed(2)}
+            </p>
+            <p className="md:text-lg text-gray-500 line-through">
+              {currency}
+              {courseData.coursePrice}
+            </p>
+            <p className="md:text-lg text-gray-500">
+              {courseData.discount}%off
+            </p>
+          </div>
+          <div className="flex items-center text-sm md:text-lg gap-4 pt-2 md:pt-4 text-gray-5--">
+            <div className="flex items-center gap-1">
+              <img src={assets.star} alt="star" />
+              <p>{calculateRating(courseData)}</p>
+            </div>
+
+            <div className="h-4 w-px bg-gray-500/40"></div>
+
+            <div className="flex items-center gap-1">
+              <img src={assets.time_clock_icon} alt="clock" />
+              <p>{calculateCourseDuration(courseData)}</p>
+            </div>
+            <div className="h-4 w-px bg-gray-500/40"></div>
+            <div className="flex items-center gap-1">
+              <img src={assets.lesson_icon} alt="clock" />
+              <p>{calculateNoOfLectures(courseData)}lessons</p>
+            </div>
+          </div>
+          <button className="md:mt-6 mt-4 w-full py-3 rounded bg-blue-600 text-white font-medium ">{isAlreadyEnrolled?'Already Enrolled':'Enroll now'}</button>
+          <div className="pt-6">
+            <p className="md:tex-xl text-lg font-medium text-gray-800">What's in the course</p>
+            <ul className="ml-4 pt-2 text-sm md:text-lg list-disc text-gra">
+              <li>Lifetime access with free updates.</li>
+              <li>Step-by-step, hands-on project guidance.</li>
+              <li>Downloadable resources and source code.</li>
+              <li>Quizzes to test your knowledge.</li>
+              <li>Certificate of completion.</li>
+              <li>Quizzes to test your knowledge.</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+      <Footer/>
+    </>
+    
+  )
+  
 };
 
 export default CourseDetails;
